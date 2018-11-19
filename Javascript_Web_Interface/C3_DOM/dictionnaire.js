486f7e309530ed15d1c7807ff8965a9fbd9d8350
Processeur <processor.com@gmail.com>
2018-11-19 19:43:02 +0100

    maj

---
+++ Javascript_Web_Interface/C3_DOM/dictionnaire.js
@@ -0,0 +1,44 @@
+// Liste des mots du dictionnaire
+var mots = [
+    {
+        terme: "Procrastination",
+        definition: "Tendance pathologique à remettre systématiquement au lendemain"
+    },
+    {
+        terme: "Tautologie",
+        definition: "Phrase dont la formulation ne peut être que vraie"
+    },
+    {
+        terme: "Oxymore",
+        definition: "Figure de style qui réunit dans un même syntagme deux termes sémantiquement opposés"
+    }
+];
+// TODO : créer le dictionnaire sur la page web, dans la div "contenu"
+document.getElementById("contenu").appendChild(document.createElement("dl"));
+for (i = 0; i<mots.length ; i++){
+    var dTitle = document.getElementById("contenu").appendChild(document.createElement("dt"));
+    var dPolice = dTitle.appendChild(document.createElement("strong"));
+    dPolice.textContent = mots[i].terme;
+    var dDefinition = document.getElementById("contenu").appendChild(document.createElement("dd"));
+    dDefinition.textContent = mots[i].definition;
+}
+
+
+// Correction
+
+var dlElt = document.createElement("dl"); // Création de la liste
+
+// Pour chaque mot, on construit une balise <dt> avec le terme et une balise <dd> avec sa définition
+mots.forEach(function (mot) {
+    var dtElt = document.createElement("dt");
+    var strongElt = document.createElement("strong");
+    strongElt.textContent = mot.terme;
+    var ddElt = document.createElement("dd");
+    ddElt.textContent = mot.definition;
+
+    dtElt.appendChild(strongElt);
+    dlElt.appendChild(dtElt);
+    dlElt.appendChild(ddElt);
+});
+
+document.getElementById("contenu").appendChild(dlElt); // Ajout de la liste à la page
