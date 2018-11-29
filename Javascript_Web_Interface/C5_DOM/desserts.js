a41d9e18614cd4d7d49f127ca5bbbff77495404f
Processeur <processor.com@gmail.com>
2018-11-29 20:01:12 +0100

    maj

--- Javascript_Web_Interface/C5_DOM/desserts.js
+++ Javascript_Web_Interface/C5_DOM/desserts.js
@@ -1,10 +1,26 @@
 var menuElt = document.getElementById("desserts");
+
+
+
 function addDessert(e){
 	var saisieElt = prompt("Veuillez entrer un de vos desserts préférés:");
 	var dessertElt = document.createElement("li");
-	dessertElt.textContent = saisieElt;
+
+	var linkElt = document.createElement("a");
+	linkElt.textContent = saisieElt;
+	linkElt.href = "#"
+	linkElt.id = saisieElt;
+
 	menuElt.appendChild(dessertElt);
+	dessertElt.appendChild(linkElt);
 };
 
 var buttonElt = document.querySelector("button");
 buttonElt.addEventListener("click", addDessert);
+
+
+
+ document.getElementsByTagName("li").addEventListener("click", function (e) {
+     console.log("Continuez plutôt à lire le cours ;)");
+     e.preventDefault(); // Annulation de la navigation vers la cible du lien
+ });
